8e626c066b8e6ce690074858d52c2bac0919ed76
HarshKohli <harsh14791@gmail.com>
2020-09-26 01:06:35 +0530

    Inference Initial Commit

--- inference.py
+++ inference.py
@@ -10,6 +10,16 @@ import os
 
 config = yaml.safe_load(open('config.yml', 'r'))
 os.environ["TORCH_HOME"] = config['base_model_dir']
-
 model = SentenceTransformer(config['save_dir'])
+
+test_file = open(config['test_file'], 'r', encoding='utf8')
+
+query_topics_map = {}
+for line in test_file.readlines():
+    info = line.split(config['delimiter'])
+    query, topic = info[0], info[1]
+    if query in query_topics_map:
+        query_topics_map[query].append(topic)
+    else:
+        query_topics_map[query] = [topic]
 
